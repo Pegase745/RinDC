1c7ce365519c9ccf2758c242395633c954f77781
Pegase745 <michel.nemnom@gmail.com>
2013-04-18 17:56:31 +0200

    CRUD api for rack management

--- routes/racks.js
+++ routes/racks.js
@@ -1,7 +1,89 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db     = mongo.Db,
+    BSON   = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true}),
+        db = new Db('rindc', server);
+
+db.open(function(err, db) {
+  if(!err) {
+    console.log("Connected to 'rindc' database");
+  }
+})
+
 exports.findAll = function(req, res) {
-	res.send('findAll');
+	db.collection('racks', function(err, collection) {
+    collection.find().toArray(function(err, items) {
+      res.send(items);
+    });
+  });
 };
 
 exports.findById = function(req, res) {
-	res.send('findById');
+	var id = req.params.id;
+
+  console.log('Retrieving rack: ' + id);
+
+  db.collection('racks', function(err, collection) {
+    collection.findOne({'_id': new BSON.ObjectID(id)}, function(err, item) {
+      res.send(item);
+    });
+  });
 };
+
+exports.addRack= function(req, res) {
+  var rack = req.body;
+  rack["creation"] = new Date();
+
+  console.log('Adding rack: ' + JSON.stringify(rack));
+
+  db.collection('racks', function(err, collection) {
+    collection.insert(rack, {safe: true}, function(err, result) {
+      if(err) {
+        res.send({'error': 'An error has occured on insert'});
+      } else {
+        console.log('Success: ' + JSON.stringify(result[0]));
+        res.send(result[0]);
+      }
+    });
+  });
+}
+
+exports.updateRack = function(req, res) {
+  var id   =  req.params.id,
+      rack =  req.body;
+
+  console.log('Updating rack: ' + id);
+  console.log(JSON.stringify(rack));
+
+  db.collection('racks', function(err, collection) {
+    collection.update({'_id': new BSON.ObjectID(id)}, rack, {safe: true}, function(err, result) {
+      if(err) {
+        console.log('Error updating rack: ' + err);
+        res.send({'error': 'An error has occured on update'});
+      } else {
+        console.log('' + result + ' rack(s) updated');
+        res.send(rack);
+      }
+    });
+  });
+}
+
+exports.deleteRack = function(req, res) {
+  var id = req.params.id;
+
+  console.log('Deleting rack: ' + id);
+
+  db.collection('racks', function(err, collection) {
+    collection.remove({'_id': new BSON.ObjectID(id)}, {safe: true}, function(err, result) {
+      if(err) {
+        res.send({'error': 'An error has occurred on delete - ' + err});
+      } else {
+        console.log('' + result + ' rack(s) deleted');
+        res.send(req.body);
+      }
+    });
+  });
+}
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express'),
     racks   = require('./routes/racks'),
     app     = express();
 
+app.use(express.bodyParser());
+
 // Routes
 app.get('/racks', racks.findAll);
 app.get('/racks/:id', racks.findById);
+app.post('/racks', racks.addRack);
+app.put('/racks/:id', racks.updateRack);
+app.delete('/racks/:id', racks.deleteRack);
 
 app.listen(3000);
 console.log('Listening on port 3000');
